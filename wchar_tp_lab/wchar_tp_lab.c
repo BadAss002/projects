f93b5fe6bd4d9ae19ce255805d34491ab2bcad71
BadAss002 <fedorovn27@gmail.com>
2026-02-16 17:53:28 +0300

    guess it works

--- wchar_tp_lab/wchar_tp_lab.c
+++ wchar_tp_lab/wchar_tp_lab.c
@@ -3,10 +3,33 @@
 #include <locale.h>
 #define LENGTH 81
 
-wchar_t* word_connector(wchar_t* first, wchar_t* second)
+wchar_t* common_part_find(wchar_t* first, wchar_t* second)
 {
-    wcscat(first,second);
-    wprintf(L"%ls\n", first);
+    wchar_t* final_ptr = 0;
+    size_t max_length;
+    size_t len_fisrt = wcslen(first);
+    size_t len_second = wcslen(second);
+
+    if (len_fisrt>len_second)
+        max_length = len_second;
+    else
+        max_length = len_fisrt;
+
+    first += len_fisrt;
+    first--;
+
+    for (size_t common_length = 1; common_length <= max_length; common_length++)
+    {
+        if (wcsncmp(first,second,common_length) == 0)
+        {
+            final_ptr = second+common_length;
+            //printf("%ld\n", common_length);
+        }
+        first--;
+    }
+
+    
+    return final_ptr;
 }
 
 int main(void)
@@ -14,13 +37,39 @@ int main(void)
     setlocale(LC_ALL, "");
     wchar_t line[LENGTH];
     wchar_t next_line[LENGTH];
+    wchar_t final_line[LENGTH];
+    wchar_t *where_to_copy;
 
-    while (wscanf(L"%ls", line) != EOF) 
+    if (wscanf(L"%ls", line) == EOF)
+        return -1;
+    if (wscanf(L"%ls", next_line) == EOF)
+        return -1;
+
+    while (1)
     {
-        if (wscanf(L"%ls", next_line) == EOF)
-            break;
-        word_connector(line, next_line);
+        where_to_copy = common_part_find(line, next_line);
+
+        if (where_to_copy)
+        {
+            wcscat(line, where_to_copy);
+            wcscat(final_line, line);
+            wcscpy(line,next_line);
+            if (wscanf(L"%ls", next_line) == EOF)
+                break;
+        }
+        else
+        {
+            wcscat(final_line, line);
+            wcscpy(line, next_line);
+            if (wscanf(L"%ls", next_line) == EOF)
+            {
+                wcscat(final_line, line);
+                break;
+            }
+        }
     }
+
+    wprintf(L"%ls\n", final_line);
 
 
     
