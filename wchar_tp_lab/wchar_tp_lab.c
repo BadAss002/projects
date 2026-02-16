318155a49c11f4275eb6ccfcbaedf1a6ca6cb657
BadAss002 <fedorovn27@gmail.com>
2026-02-16 12:31:39 +0300

    wchar_lab_base

--- wchar_tp_lab/wchar_tp_lab.c
+++ wchar_tp_lab/wchar_tp_lab.c
@@ -1,15 +1,33 @@
 #include <stdio.h>
 #include <wchar.h>
 #include <locale.h>
+#define LENGTH 81
+
+wchar_t* word_connector(wchar_t* first, wchar_t* second)
+{
+    wcscat(first,second);
+    wprintf(L"%ls\n", first);
+}
 
 int main(void)
 {
     setlocale(LC_ALL, "");
-    wchar_t a[100];
-    /* wscanf(L"%ls", a);
-    wprintf(L"%ls\n", a); */
-    printf("%d\n", sizeof(wchar_t));
-    //commit2
+    wchar_t line[LENGTH];
+    wchar_t next_line[LENGTH];
+
+    while (wscanf(L"%ls", line) != EOF) 
+    {
+        if (wscanf(L"%ls", next_line) == EOF)
+            break;
+        word_connector(line, next_line);
+    }
+
+
+    
+
+
+
+
 
 
     return 0;
