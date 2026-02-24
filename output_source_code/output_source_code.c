8b6309f45e8dcd0bd8e2d405aadf0142ba5baf10
BadAss002 <fedorovn27@gmail.com>
2026-02-24 15:35:01 +0300

    output_source_lab_1

---
+++ output_source_code/output_source_code.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#define LENGTH 100
+
+int main(void)
+{
+    FILE* input;
+    input = fopen("output_source_code.c", "r");
+
+    char ch;
+
+    while((ch = fgetc(input)) != EOF)
+    {
+        printf("%c", ch);
+    }
+
+    return 0;
+}
