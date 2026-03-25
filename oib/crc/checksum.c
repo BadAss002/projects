412347665a58a6284cdf76c200f93018300b14c9
Daniil Velichko <tourphone.spb@gmail.com>
2026-03-25 15:08:44 +0300

    small changes

--- oib/crc/checksum.c
+++ oib/crc/checksum.c
@@ -7,7 +7,7 @@
 #include <stddef.h>
 
 
-#define MAX_LENGTH 40000
+#define MAX_LENGTH 450000000
 #define const_poly  0xEDB88320u
 #define CRC_WIDTH 32
 #define POLY 0x104C11DB7u
@@ -33,7 +33,7 @@ int checksum(char * text,int current_length)
     return C;
 }
 
-// CRC-32 как нахуй просят в методичке, математически верный на 1000%(поменять глобальную poly на 0x04C11DB7u)
+// CRC-32 как просят в методичке, математически верный на 1000%(поменять глобальную poly на 0x04C11DB7u)
 // uint32_t crc32(const unsigned char *text, size_t current_length)
 // {
 //     uint32_t crc = 0xFFFFFFFFu;
@@ -54,100 +54,100 @@ int checksum(char * text,int current_length)
 //     return crc ^ 0xFFFFFFFF;
 // }
 //delenie
-uint32_t crc32(uint8_t* data, size_t len) {
-    size_t total_bits = len * 8 + CRC_WIDTH;
-    size_t total_bytes = (total_bits + 7) / 8;
+// uint32_t crc32(uint8_t* data, size_t len) {
+//     size_t total_bits = len * 8 + CRC_WIDTH;
+//     size_t total_bytes = (total_bits + 7) / 8;
 
-    // Буфер с исходными данными + 32 нуля (CRC_WIDTH)
-    uint8_t* message = calloc(total_bytes, sizeof(uint8_t));
-    if (!message) return 0;
-    memcpy(message, data, len);
+//     // Буфер с исходными данными + 32 нуля (CRC_WIDTH)
+//     uint8_t* message = calloc(total_bytes, sizeof(uint8_t));
+//     if (!message) return 0;
+//     memcpy(message, data, len);
 
-    // Делимое в битах
-    uint8_t* dividend = malloc(total_bytes);
-    if (!dividend) { free(message); return 0; }
-    memcpy(dividend, message, total_bytes);
+//     // Делимое в битах
+//     uint8_t* dividend = malloc(total_bytes);
+//     if (!dividend) { free(message); return 0; }
+//     memcpy(dividend, message, total_bytes);
 
-    // Деление в столбик по битам
-    for (size_t bit_pos = 0; bit_pos <= total_bits - (CRC_WIDTH + 1); bit_pos++) {
-        size_t byte_idx = bit_pos / 8;
-        int bit_in_byte = 7 - (bit_pos % 8);
+//     // Деление в столбик по битам
+//     for (size_t bit_pos = 0; bit_pos <= total_bits - (CRC_WIDTH + 1); bit_pos++) {
+//         size_t byte_idx = bit_pos / 8;
+//         int bit_in_byte = 7 - (bit_pos % 8);
 
-        int current_bit = (dividend[byte_idx] >> bit_in_byte) & 1;
+//         int current_bit = (dividend[byte_idx] >> bit_in_byte) & 1;
 
-        if (current_bit) {
-            // XOR по всем битам полинома
-            for (int i = 0; i < CRC_WIDTH + 1; i++) {
-                size_t xor_byte = (bit_pos + i) / 8;
-                int xor_bit = 7 - ((bit_pos + i) % 8);
+//         if (current_bit) {
+//             // XOR по всем битам полинома
+//             for (int i = 0; i < CRC_WIDTH + 1; i++) {
+//                 size_t xor_byte = (bit_pos + i) / 8;
+//                 int xor_bit = 7 - ((bit_pos + i) % 8);
 
-                int poly_bit = (POLY >> (CRC_WIDTH - i)) & 1;
-                if (poly_bit) dividend[xor_byte] ^= (1 << xor_bit);
-            }
-        }
-    }
+//                 int poly_bit = (POLY >> (CRC_WIDTH - i)) & 1;
+//                 if (poly_bit) dividend[xor_byte] ^= (1 << xor_bit);
+//             }
+//         }
+//     }
 
-    // Извлечение CRC (последние 32 бита)
-    uint32_t crc = 0;
-    for (int i = 0; i < CRC_WIDTH; i++) {
-        size_t bit_pos = total_bits - CRC_WIDTH + i;
-        size_t byte_idx = bit_pos / 8;
-        int bit_in_byte = 7 - (bit_pos % 8);
-        if ((dividend[byte_idx] >> bit_in_byte) & 1) {
-            crc |= (1 << (CRC_WIDTH - 1 - i));
-        }
-    }
+//     // Извлечение CRC (последние 32 бита)
+//     uint32_t crc = 0;
+//     for (int i = 0; i < CRC_WIDTH; i++) {
+//         size_t bit_pos = total_bits - CRC_WIDTH + i;
+//         size_t byte_idx = bit_pos / 8;
+//         int bit_in_byte = 7 - (bit_pos % 8);
+//         if ((dividend[byte_idx] >> bit_in_byte) & 1) {
+//             crc |= (1 << (CRC_WIDTH - 1 - i));
+//         }
+//     }
 
-    free(message);
-    free(dividend);
+//     free(message);
+//     free(dividend);
 
-    return crc;
-}
+//     return crc;
+// }
 
 // CRC-32 как в стандарте, крутой классный, переносимый, одобряемый
-// uint32_t crc32(const unsigned char *text, size_t current_length)
-// {
-//     uint32_t crc = 0xFFFFFFFFu;
+uint32_t crc32(const unsigned char *text, size_t current_length)
+{
+    uint32_t crc = 0xFFFFFFFFu;
 
-//     for (size_t i=0;i<current_length; i++)
-//     {
-//         crc ^= (uint32_t)text[i];
-//         for(int j = 0; j < 8; j++)
-//         {
-//             if(crc & 1)
-//                 crc = (crc >> 1) ^ const_poly;
-//             else
-//                 crc >>= 1;
-//         }
-//     }
+    for (size_t i=0;i<current_length; i++)
+    {
+        crc ^= (uint32_t)text[i];
+        for(int j = 0; j < 8; j++)
+        {
+            if(crc & 1)
+                crc = (crc >> 1) ^ const_poly;
+            else
+                crc >>= 1;
+        }
+    }
 
-//     return crc ^ 0xFFFFFFFFu;
-// }
+    return crc ^ 0xFFFFFFFFu;
+}
 //0x82F63B78 - замена для 15
-// unsigned int crc_file(const char *filename)
-// {
-//     FILE *fp = fopen(filename, "rb");
-//     if (!fp) return 0;
+unsigned int crc_file(const char *filename)
+{
+    FILE *fp = fopen(filename, "rb");
+    if (!fp) return 0;
 
-//     unsigned int crc = 0xFFFFFFFF;
-//     int c;
+    unsigned int crc = 0xFFFFFFFF;
+    int c;
 
-//     while ((c = fgetc(fp)) != EOF)
-//     {
-//         crc ^= (unsigned char)c;
+    while ((c = fgetc(fp)) != EOF)
+    {
+        crc ^= (unsigned char)c;
 
-//         for (int i = 0; i < 8; i++)
-//         {
-//             if (crc & 1)
-//                 crc = (crc >> 1) ^ 0xEDB88320;
-//             else
-//                 crc >>= 1;
-//         }
-//     }
+        for (int i = 0; i < 8; i++)
+        {
+            if (crc & 1)
+                crc = (crc >> 1) ^ 0xEDB88320;
+            else
+                crc >>= 1;
+        }
+    }
 
-//     fclose(fp);
-//     return crc;
-// } хуйня
+    fclose(fp);
+    return crc;
+} 
 void print_line(char * text,int current_length)
 {
     for (int i=0;i<current_length;i++)
@@ -155,19 +155,26 @@ void print_line(char * text,int current_length)
     printf("\n\n");
 }
 
-void line_double(char * text)
-{
-    for (int i=0;i<length;i++)
-    {
-        text[length+i] = text[i];
+void line_double_safe(char *text, size_t text_size, const char *src, size_t src_len) {
+    if (2 * src_len > text_size) {
+        fprintf(stderr, "Error: buffer too small to double the string\n");
+        exit(1);
+    }
+    for (size_t i = 0; i < src_len; i++) {
+        text[i] = src[i];
+        text[src_len + i] = src[i];
     }
 }
+
 
 int main(void)
 {
     srand(time(NULL));
 
     f = fopen("test.txt","rb");
+    if (!f) {
+    perror("Cannot open file");
+    return 1;}
 
     unsigned char text[MAX_LENGTH];
     length = fread(text, 1, MAX_LENGTH, f);
@@ -222,7 +229,9 @@ int main(void)
     text[second_ch] = temp;
 
     print_line(text,length);
-    printf("\t%c(%d) <-> %c(%d)\n\n",text,text[first_ch],first_ch,text[second_ch],second_ch);
+    printf("\t%c(%d) <-> %c(%d)\n\n",
+       text[first_ch], first_ch,
+       text[second_ch], second_ch);
 
     int c4 = checksum(text,length); //контрольная сумма с перестановкой символов
     unsigned int s4 = crc32(text,length);
